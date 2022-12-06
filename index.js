4a4eadda0c400a97623c47164a51ba29300cd923
zaynbsn <nils.vainui@gmail.com>
2022-12-06 17:54:49 +0100

    add vercel config

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const path = require('path')
 
 const app = express()
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/pages/index.html'))
+  // res.sendFile(path.join(__dirname, '/public/pages/index.html'))
+  res.sendFile('/pages/index.html')
 })
 
 app.listen(3000, () => {
